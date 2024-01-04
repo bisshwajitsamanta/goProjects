58575167ebc99efb17e5b446c15ff2d8502ff715
bisshwajitsamanta <bisshwajitsamanta1689@gmail.com>
2024-01-04 13:10:39 +0530

    Initial changes

---
+++ connect/README.md
@@ -0,0 +1,11 @@
+# switch
+CLI for Multipurpose Utility
+
+Plan to include purpose
+
+* Switch Status -> 
+  * Shows the current aws credentials account
+  * Shows the current context if selected
+  * Shows the current namespace if selected any
+* Switch cluster-name <shorthand for cluster>
+  * Switch to the mentioned cluster
---
+++ connect/cmd/root.go
@@ -0,0 +1,37 @@
+// Package cmd - Root.go file contains root command that will run if no sub command exist/*
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+)
+
+var cfgFile string
+
+var RootCmd = &cobra.Command{
+	Use:   "switch",
+	Short: "Utility Tool",
+	Long:  `Give Devops Guys capability to switch between sessions`,
+}
+
+func Execute() {
+	if err := RootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	/*
+			Here you will define your flags and configuration settings Cobra supports persistent flags,
+		    which, if defined here will be global for your application.
+	*/
+
+	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (Default is $HOME/.switch.yaml")
+
+	/*
+	 Cobra also supports local flags, which will only run when this action is called directly.
+	*/
+}
